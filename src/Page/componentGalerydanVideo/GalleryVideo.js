519ca241f9941cb0f27e7e2a8c869c6b0a2c1fbb
Phuasamara <phua0205@gmail.com>
2022-04-08 14:22:59 +0700

    video

--- src/Page/componentGalerydanVideo/GalleryVideo.js
+++ src/Page/componentGalerydanVideo/GalleryVideo.js
@@ -9,24 +9,72 @@ import AccessTimeIcon from '@mui/icons-material/AccessTime';
 function GalleryVideo(){
     return(
         <div>
-        <Grid container style={{marginTop: 1}}>
+        <Grid container spacing={4} style={{paddingLeft: 50, paddingRight: 50, paddingBottom: 30, marginTop: 1}}>
         <p style={{fontSize: 15}}>Galeri & News / News</p>
             <Grid item xs={12} sm={12}>
-                <p style={{fontSize: 50, marginBottom: 10, marginTop: 20, fontWeight: 'bold'}}>Gallery Video</p>
+                <p style={{fontSize: 50, marginBottom: 10, marginTop: 0, fontWeight: 'bold'}}>Gallery Video</p>
             </Grid>
-            <Grid item xs={12} sm={6}>
-                <div>
+            <Grid item xs={12} sm={6} style={{height: '90%', width: '90%'}}> 
+                <div style={{boxShadow: "5px 5px 5px 5px #9E9E9E"}}>
                 <ReactPlayer
                 url="https://youtu.be/ML5bCZ2DCYE"
                 width="100%"
-                height="450px"
+                height="350px"
                 playing
                 playIcon= {<PlayCircleOutlineIcon style={{color: 'white', fontSize: 150}} /> }
                 light={Galery1}
 />
-                <div style={{backgroundColor: 'white'}}>
-                    <p style={{margin: 0, fontSize: 20}}>Video Tittle</p>
-                    <p style={{margin: 0}}><AccessTimeIcon/>23/3/3</p>
+                <div style={{backgroundColor: 'white', display: 'flex', justifyContent: 'space-between', alignItems: 'center', height: 100}}>
+                    <p style={{margin: 0, fontSize: 20, display: 'flex', alignItems: 'center', justifyContent: 'center', paddingLeft: 20}}>Video Tittle</p>
+                    <p style={{margin: 0, color:'#9E9E9E', display: 'flex', alignItems: 'center', paddingRight: 20}}><AccessTimeIcon color="disabled" sx={{ fontSize: 15, marginRight: 1 }}/>23/3/3</p>
+                </div>
+                </div>
+            </Grid>
+            <Grid item xs={12} sm={6} style={{height: '90%', width: '90%'}}> 
+                <div style={{boxShadow: "5px 5px 5px 5px #9E9E9E"}}>
+                <ReactPlayer
+                url="https://youtu.be/ML5bCZ2DCYE"
+                width="100%"
+                height="350px"
+                playing
+                playIcon= {<PlayCircleOutlineIcon style={{color: 'white', fontSize: 150}} /> }
+                light={Galery1}
+/>
+                <div style={{backgroundColor: 'white', display: 'flex', justifyContent: 'space-between', alignItems: 'center', height: 100}}>
+                    <p style={{margin: 0, fontSize: 20, display: 'flex', alignItems: 'center', justifyContent: 'center', paddingLeft: 20}}>Video Tittle</p>
+                    <p style={{margin: 0, color:'#9E9E9E', display: 'flex', alignItems: 'center', paddingRight: 20}}><AccessTimeIcon color="disabled" sx={{ fontSize: 15, marginRight: 1 }}/>23/3/3</p>
+                </div>
+                </div>
+            </Grid>
+            <Grid item xs={12} sm={6} style={{height: '90%', width: '90%'}}> 
+                <div style={{boxShadow: "5px 5px 5px 5px #9E9E9E"}}>
+                <ReactPlayer
+                url="https://youtu.be/ML5bCZ2DCYE"
+                width="100%"
+                height="350px"
+                playing
+                playIcon= {<PlayCircleOutlineIcon style={{color: 'white', fontSize: 150}} /> }
+                light={Galery1}
+/>
+                <div style={{backgroundColor: 'white', display: 'flex', justifyContent: 'space-between', alignItems: 'center', height: 100}}>
+                    <p style={{margin: 0, fontSize: 20, display: 'flex', alignItems: 'center', justifyContent: 'center', paddingLeft: 20}}>Video Tittle</p>
+                    <p style={{margin: 0, color:'#9E9E9E', display: 'flex', alignItems: 'center', paddingRight: 20}}><AccessTimeIcon color="disabled" sx={{ fontSize: 15, marginRight: 1 }}/>23/3/3</p>
+                </div>
+                </div>
+            </Grid>
+            <Grid item xs={12} sm={6} style={{height: '90%', width: '90%'}}> 
+                <div style={{boxShadow: "5px 5px 5px 5px #9E9E9E"}}>
+                <ReactPlayer
+                url="https://youtu.be/ML5bCZ2DCYE"
+                width="100%"
+                height="350px"
+                playing
+                playIcon= {<PlayCircleOutlineIcon style={{color: 'white', fontSize: 150}} /> }
+                light={Galery1}
+/>
+                <div style={{backgroundColor: 'white', display: 'flex', justifyContent: 'space-between', alignItems: 'center', height: 100}}>
+                    <p style={{margin: 0, fontSize: 20, display: 'flex', alignItems: 'center', justifyContent: 'center', paddingLeft: 20}}>Video Tittle</p>
+                    <p style={{margin: 0, color:'#9E9E9E', display: 'flex', alignItems: 'center', paddingRight: 20}}><AccessTimeIcon color="disabled" sx={{ fontSize: 15, marginRight: 1 }}/>23/3/3</p>
                 </div>
                 </div>
             </Grid>
