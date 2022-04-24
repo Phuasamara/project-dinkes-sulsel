7227b44148953293cdae8c08e0091caefe07f5b4
Phuasamara <phua0205@gmail.com>
2022-04-24 19:16:05 +0700

    24-04-2022

--- src/Page/Survei.js
+++ src/Page/Survei.js
@@ -12,6 +12,10 @@ import Select from '@mui/material/Select';
 import Checkbox from '@mui/material/Checkbox';
 import Divider from '@mui/material/Divider';
 import Button from '@mui/material/Button';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import '../responsive.css';
 
 function Survei(){
     const [jeniskelamin, setjeniskelamin] = React.useState('');
@@ -32,11 +36,7 @@ function Survei(){
       setpekerjaan(event.target.value);
     };
 
-    const [value, setValue] = React.useState(new Date('2014-08-18T21:11:54'));
-      
-    const dateChange = (newValue) => {
-        setValue(newValue);
-    };
+    const [value, setValue] = React.useState(null);
 
     return(
         <>
@@ -44,9 +44,9 @@ function Survei(){
             <p style={{fontSize: 30, fontWeight: 'bold'}}>
                 Survei Pelayanan Publik
             </p>
-            <Grid container sx={{paddingLeft:10, paddingBottom: 5}}>
-            <div style={{boxShadow: "5px 5px 5px 5px #9E9E9E", width: '100%', display: 'flex', padding: 10}}>
-                <Grid item xs={12} sm={6} sx={{zIndex: 0, paddingRight: 10}}>
+            <Grid container sx={{paddingLeft:5, paddingBottom: 5}}>
+            <div style={{boxShadow: "5px 5px 5px 5px #9E9E9E", width: '100%', padding: 10}} className="survei-form">
+                <Grid item xs={12} sm={6} className="survei-formLeft">
                     <p>
                         <TextField fullWidth id="name" label="Nama" variant="outlined" />
                     </p>
@@ -67,10 +67,21 @@ function Survei(){
                 </Select>
                 </FormControl>
                 <p>
-                    <TextField fullWidth id="tanggallahir" label="Tanggal Lahir" variant="outlined" />
+                <FormControl fullWidth>
+                <LocalizationProvider fullWidth dateAdapter={AdapterDateFns}>
+                <DatePicker
+                    label="Tanggal Lahir"
+                    value={value}
+                    onChange={(newValue) => {
+                    setValue(newValue);
+                }}
+                renderInput={(params) => <TextField {...params} />}
+                />
+                </LocalizationProvider>
+                </FormControl>
                 </p>
                 </Grid>
-                <Grid item xs={12} sm={6} sx={{zIndex: 0, paddingLeft: 5}}>
+                <Grid item xs={12} sm={6} className="survei-formRight">
                     <p>
                     <FormControl fullWidth>
                     <InputLabel id="demo-simple-select-label">Pendidikan</InputLabel>
@@ -110,18 +121,18 @@ function Survei(){
                 </Grid>
                 </div>
             </Grid>
-            <div style={{marginLeft: 80}}>
+            <div style={{marginLeft: 40}}>
                 <p style={{fontSize: 25, fontWeight: 'bold'}}>
                     Pertanyaan-pertanyaan Kuisoner
                 </p>
             </div>
-            <Grid container sx={{paddingLeft:10, paddingBottom: 5, backgrounColor: 'white'}}>
+            <Grid container sx={{paddingLeft:5, paddingBottom: 5, backgrounColor: 'white'}}>
             <div style={{boxShadow: "5px 5px 5px 5px #9E9E9E", width: '100%', padding: 10}}>
-                <div style={{ display: 'flex',}}>
-                <Grid item xs={12} sm={7} sx={{zIndex: 0, marginRight: 10}}>
+                <div className="survei-form">
+                <Grid item xs={12} sm={6} sx={{zIndex: 0, marginRight: 10}}>
                     <p>1. Menurut anda apakah kejelasan dan kepastian petugas yang melayani di Dinas Kesehatan benar-benar sudah sesuai ?</p>
                 </Grid>
-                <Grid item xs={12} sm={5} sx={{zIndex: 0,}}>
+                <Grid item xs={12} sm={6} sx={{zIndex: 0,}}>
                     <p>
                     <FormControl>
                     <RadioGroup
@@ -151,7 +162,7 @@ function Survei(){
                     <FormControlLabel
                         value="tidakbaik"
                         control={<Radio />}
-                        label="Tidak Baik"
+                        label="Buruk"
                         labelPlacement="top"
                     />
                     </RadioGroup>
@@ -162,11 +173,11 @@ function Survei(){
                 <Grid item xs={12} sm={12} sx={{zIndex: 0,}}>
                     <Divider />
                 </Grid>
-                <div style={{ display: 'flex',}}>
-                <Grid item xs={12} sm={7} sx={{zIndex: 0, marginRight: 10}}>
+                <div className="survei-form">
+                <Grid item xs={12} sm={6} sx={{zIndex: 0, marginRight: 10}}>
                     <p>2. Apakah kesesuaian persyaratan pelayanan dengan jenis pelayanannya yang anda rasakan sudah benar-benar sesuai?</p>
                 </Grid>
-                <Grid item xs={12} sm={5} sx={{zIndex: 0,}}>
+                <Grid item xs={12} sm={6} sx={{zIndex: 0,}}>
                     <p>
                     <FormControl>
                     <RadioGroup
@@ -196,7 +207,7 @@ function Survei(){
                     <FormControlLabel
                         value="tidakbaik"
                         control={<Radio />}
-                        label="Tidak Baik"
+                        label="Buruk"
                         labelPlacement="top"
                     />
                     </RadioGroup>
@@ -207,11 +218,11 @@ function Survei(){
                 <Grid item xs={12} sm={12} sx={{zIndex: 0,}}>
                     <Divider />
                 </Grid>
-                <div style={{ display: 'flex',}}>
-                <Grid item xs={12} sm={7} sx={{zIndex: 0, marginRight: 10}}>
+                <div className="survei-form">
+                <Grid item xs={12} sm={6} sx={{zIndex: 0, marginRight: 10}}>
                     <p>3. Bagaimana tingkat kepuasan anda terkait kemudahan dalam hal tata cara atau prosedur pelayanan, kepastian pelayanan dan aspek keterbukaan (transparansi) di Dinas Kesehatan Sulawesi Selatan ?</p>
                 </Grid>
-                <Grid item xs={12} sm={5} sx={{zIndex: 0,}}>
+                <Grid item xs={12} sm={6} sx={{zIndex: 0,}}>
                     <p>
                     <FormControl>
                     <RadioGroup
@@ -241,7 +252,7 @@ function Survei(){
                     <FormControlLabel
                         value="tidakbaik"
                         control={<Radio />}
-                        label="Tidak Baik"
+                        label="Buruk"
                         labelPlacement="top"
                     />
                     </RadioGroup>
@@ -256,11 +267,11 @@ function Survei(){
                     <p style={{color: '#73726f'}}>Tuliskan komentar/usulan Saudara terhadap kemajuan dan pengembangan pelayanan informasi publik</p>
                 </Grid>
                 <Grid item xs={12} sm={12} sx={{zIndex: 0,}}>
-                    <textarea style={{ height: 200, width: 800}}/>
+                    <textarea style={{ height: 'auto', width: '100%'}}/>
                 </Grid>
             </div>
             </Grid>
-            <div style={{paddingLeft: 80, paddingBottom: 20, }}>
+            <div style={{paddingLeft: 40, paddingBottom: 20, }}>
                 <Button variant="contained" size="medium" sx={{'&:hover': {backgroundColor: '#F5b400'}, backgroundColor:'#F5b400'}}>Kirim Survei</Button>
             </div>
         </div>
